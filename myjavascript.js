18f2a24ab8b7737cf34657bebc2404f7f12b3e7f
Kamal <kamal021099@gmail.com>
2021-06-06 21:14:07 +0530

    added grid size from user and the container is now in new div to easily delete

--- myjavascript.js
+++ myjavascript.js
@@ -1,44 +1,89 @@
-
-// let size;
-// function gridSize()
-// {
-//   prompt("Enter the new size of the grid")
-// }
-
-const container = document.querySelector(".container");
-
+const gridContainer = document.querySelector(".grid-container");
+let size=16;
+let root = document.querySelector(":root");
+root.style.setProperty("--size",size);
 
 // its the grid
-for(let i=0; i<16; i++)
-{
-    for(let i=0; i<16; i++)
-
+function makeGrid(){
+    let container = document.createElement('div');
+    container.classList.add("container");
+    
+    for(let i=0; i<size; i++)
     {
-        let box=document.createElement('div');
-        box.classList.add("box")
-        // box.innerText = "box";
-        container.appendChild(box);
-
+        for(let j=0; j<size; j++)
+    
+        {
+            let box=document.createElement('div');
+            box.classList.add("box");
+            container.appendChild(box);
+    
+        }
     }
+    gridContainer.appendChild(container);
+}
+
+function addHover(){
+    //adding hover event to all the boxes.
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('mouseenter', () => {
+            box.style.backgroundColor = "blue";
+        })
+    })       
+}
+
+//reset the grid.
+function resetBox()
+{
+    boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => {
+        box.style.backgroundColor = "white";
+    })
+}
+
+function gridSize()
+{
+  size = prompt("Enter the new size of the grid(1-100");
+  
+  if(size<=50 && size>=1)
+  {
+    root.style.setProperty('--size',size);
+    let container = document.querySelector(".container");
+    container.remove();
+    resetBox();
+    makeGrid();
+    addHover();
+  }
+  else{
+      alert("dont be a smartass.");
+  }
+
 }
 
 
-//adding hover event to all the boxes.
-const boxes = document.querySelectorAll('.box');
+makeGrid();
+addHover();
 
-boxes.forEach(box => {
-box.addEventListener('mouseenter', () => {
-    box.style.backgroundColor = "blue";
-})
-})
-
-
-// reset the grid.
 const reset = document.querySelector("#reset");
-reset.addEventListener('click', () => {
-boxes.forEach(box => {
-    box.style.backgroundColor = "white";
-})
-})
+reset.addEventListener('click',resetBox)
 
-//clear the current grid and prompt for new size.
+// //clear the current grid and prompt for new size.
+const clear= document.querySelector("#clear");
+clear.addEventListener('click', gridSize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
